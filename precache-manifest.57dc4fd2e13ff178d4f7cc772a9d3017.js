760647338871b6214aaa3391b97cbe5939073f14
Tiger T. Pisnupoomi <intaniger@gmail.com>
2019-11-02 07:50:44 +0700

    Updates

---
+++ precache-manifest.57dc4fd2e13ff178d4f7cc772a9d3017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15aca9a14c93dbf8edb245eda9a9bbc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3699d18180aec4cf60d5",
+    "url": "/static/css/main.20e8513c.chunk.css"
+  },
+  {
+    "revision": "d61a636ce1eb3c6309bd",
+    "url": "/static/js/2.3e4d0c58.chunk.js"
+  },
+  {
+    "revision": "3699d18180aec4cf60d5",
+    "url": "/static/js/main.bba9b954.chunk.js"
+  },
+  {
+    "revision": "e07cc3766c2fec5b61b0",
+    "url": "/static/js/runtime-main.82ade478.js"
+  }
+]);
